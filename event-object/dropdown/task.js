433cbf94ae9ffa35a090ed7a45731dbc14d0f66b
Mariya <mariya.shestakova.777@gmail.com>
2025-04-16 13:32:42 +0300

    first commit dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,27 +1,29 @@
-let value = document.querySelector(" .dropdown__value");
-const list = document.querySelector(" .dropdown__list");
-let items = document.querySelectorAll(" .dropdown__item");
+const dropdowns = document.querySelectorAll('.dropdown');
 
+dropdowns.forEach(dropdown => {
+    const value = dropdown.querySelector('.dropdown__value');
+    const list = dropdown.querySelector('.dropdown__list');
+    const items = dropdown.querySelectorAll('.dropdown__item');
 
-value.addEventListener('click', function() {
-    list.classList.add("dropdown__list_active");
+     value.addEventListener('click', () => {
+        list.classList.add('dropdown__list_active');
+    });
 
-})
+    items.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
 
-items.forEach((dropdown__item) => {
-    dropdown__item.addEventListener('click', function(event){
-        event.preventDefault();
-        const selectedValue = this.textContent;
-        value.textContent = selectedValue;
-        list.classList.remove("dropdown_list_active")
-    })
-})
+            const selectedValue = item.textContent;
+            value.textContent = selectedValue;
 
-document.addEventListener('click', function(event){
-    if (!event.target.closest('.dropdown')){
-        list.classList.remove("dropdown__list_active")
-    }
-})
+            list.classList.remove('dropdown__list_active');
+        });
+    });
 
-
+    document.addEventListener('click', (event) => {
+        if (!event.target.closest('.dropdown')){
+            list.classList.remove("dropdown__list_active")
+        }
+    });
+});
 
